1ef5b0f98acd4b58988e330cba49be62821e9a6e
jomaperez60 <134015782+jomaperez60@users.noreply.github.com>
2026-03-22 10:22:45 -0600

    Desde antigravity

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'med-v2';
+const assets = ['./', 'index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
